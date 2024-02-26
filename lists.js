d7d1bbcc3dbdd3baa05416d5a6670d7f7c74b7b9
brynsgtn <bryansuguitan17@gmail.com>
2024-02-26 15:22:31 -0600

    Finalized project

--- lists.js
+++ lists.js
@@ -1,24 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove, get, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+// Firebase Link
 const appSettings = {
     databaseURL: "https://to-do-list-6850c-default-rtdb.firebaseio.com/"
 }
 
+// Database initialization and connection
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const listItemsInDB = ref(database, "listItems")
-const listItemsEl = document.getElementById("list-items");
 
+// Variable declaration and initialization
+
+const listItemsEl = document.getElementById("list-items");
 const numItemsEl = document.getElementById("numItems")
 let selectedItemID = null;
-
-
+const backBtn = document.getElementById("back-btn");
 const listNameInputFieldEl = document.getElementById("list-name");
-
 const addListButtonEl = document.getElementById("add-list")
+let isTrue = true
+const editBtnEl = document.getElementById("edit-list") 
 
 
+// Event listener for adding a task
 addListButtonEl.addEventListener("click", function() {
     let inputValue = listNameInputFieldEl.value
 
@@ -28,24 +33,22 @@ addListButtonEl.addEventListener("click", function() {
     } else if (inputValue.length > 15) {
         alert("Maximum of 15 characters only!")
     } else {
-        alert("Input Required!");
+        alert("Input required!");
     }
 
     clearlistNameInputField();
     
 })
 
+// Function for rendering the list whenever there are changes
 onValue(listItemsInDB, function(snapshot) {
 
     if (snapshot.exists()) {
         let listItemsArray = Object.entries(snapshot.val())
-        
         clearItemListsEl()
     
         for(let i = 0; i < listItemsArray.length; i++) {
             let currentItem = listItemsArray[i]
-            let currentItemID = listItemsArray[0]
-            let currentItemValue = listItemsArray[1]
             appendlistItemsEl(currentItem)
 
             let totalItems = listItemsArray.length
@@ -60,10 +63,12 @@ onValue(listItemsInDB, function(snapshot) {
 
 })
 
+// Function to clear input field
 function clearlistNameInputField() {
     listNameInputFieldEl.value = ""
 }
 
+// Function that render the list
 function appendlistItemsEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
@@ -71,7 +76,6 @@ function appendlistItemsEl(item) {
     // Create new elements
     let newEl = document.createElement("li");
     let newDivEl = document.createElement("div");
-
     let deleteButtonEl = document.createElement("button");
     let pencilButtonEl = document.createElement("button");
     let deleteImgEl = document.createElement("img");
@@ -80,8 +84,6 @@ function appendlistItemsEl(item) {
     // Set text content for the li element
     newEl.textContent = itemValue;
 
-
-
     // Set classes and attributes for the buttons
     deleteButtonEl.className = "delete-btn";
     pencilButtonEl.className = "pencil-btn";
@@ -89,11 +91,11 @@ function appendlistItemsEl(item) {
     pencilButtonEl.setAttribute("data-bs-toggle", "modal");
     pencilButtonEl.setAttribute("data-bs-target", "#editDeleteItemModal");
 
-    // Set alt text and source for the images
+    // Set source for the images
     deleteImgEl.className = "img-delete";
-    deleteImgEl.src = "./images/delete.png";
+    deleteImgEl.src = "./delete.png";
     pencilImgEl.className = "img-pencil";
-    pencilImgEl.src = "./images/pencil.png";
+    pencilImgEl.src = "./pencil.png";
 
     // Append images to buttons
     deleteButtonEl.appendChild(deleteImgEl);
@@ -113,58 +115,56 @@ function appendlistItemsEl(item) {
         remove(exactLocationOfItemInDB);
     });
 
-    
+    // Add event listener to edit button
     pencilButtonEl.addEventListener("click", function(){
         let inputElement = document.getElementById("edit-list-name");
-    inputElement.value = itemValue;
+        inputElement.value = itemValue;
 
     // Store the ID of the selected item
-    selectedItemID = itemID;
-
- 
+        selectedItemID = itemID; 
     })
     
-
-
-   let isTrue = true
+    // Add event listener to a task to strike out text and change background
     newEl.addEventListener("click", function() {
-        console.log("clicked!")
-        console.log(itemID)
-        isTrue = !isTrue;
+        
         newEl.className = (isTrue ? 'clicked' : 'unclicked'); 
+        isTrue = !isTrue;
    
     })
 
     // Append li to listItemsEl
     listItemsEl.appendChild(newEl);
-
-
 }
 
-let editBtnEl = document.getElementById("edit-list") 
 
+// Add event listener to edit button
 editBtnEl.addEventListener("click", function() {
      // Retrieve the new value from the input field
      let newValue = document.getElementById("edit-list-name").value;
-    if (selectedItemID) {
-       
-        if (newValue.length > 1 && newValue.length <= 15) {
+     console.log(selectedItemID)
+        if (selectedItemID) {
+        
+            if (newValue.length > 1 && newValue.length <= 15) {
                 // Update the value of the selected item in the database
-                console.log (newValue.length)
-            let exactLocationOfItemInDB = ref(database, `listItems/${selectedItemID}`);
-            set(exactLocationOfItemInDB, newValue);
-                        // Reset the selectedItemID after editing
-        selectedItemID = null;
-        } else if (newValue.length > 15) {
-            alert("Maximum of 15 characters only!");
-        } else {
-            alert("Input Required!");
-            console.log (newValue.length)
+                let exactLocationOfItemInDB = ref(database, `listItems/${selectedItemID}`);
+                set(exactLocationOfItemInDB, newValue);
+                            
+                // Reset the selectedItemID after editing
+                selectedItemID = null;
+            } else if (newValue.length > 15) {
+                alert("Maximum of 15 characters only!");
+            } else {
+                alert("Input required!");
+            }
         }
-    }
 });
 
+// Function to clear list
 function clearItemListsEl() {
       listItemsEl.innerHTML = "";
 }
 
+// Add event to go back to landing page
+backBtn.addEventListener("click", function() {
+    location.href="./index.html"
+})
